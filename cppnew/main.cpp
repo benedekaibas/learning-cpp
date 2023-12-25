262011250da878a53cbfd8e83400de50b9f62140
kaibas01 <kaibas01@allegheny.edu>
2023-12-25 16:35:15 -0500

    done todo

--- cppnew/main.cpp
+++ cppnew/main.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 
 int main(){
-    cout << "Hello User!";
+    cout << "Hello User!" << "\n";
 
 
     // TODO: Make this better and work.
-    // The concept has to stay the same 
     float a = 7;
     float b = 7;
+
     string operand;
     cout << "Enter an operand that you would like to do: ";
     cin >> operand;
     
     if (operand == "+"){
-        cout << a + b;
+        cout << "result:" << a + b;
     } else if (operand == "-"){
-        cout << a - b;
+        cout << "result" << a - b;
     } else if (operand == "/"){
-        cout << a / b;
+        cout << "result"  << a / b;
     } else if (operand == "*"){
-        cout << a * b;
+        cout << "result" << a * b;
     }else {
         cout << "You did not enter anything or not an operand";
     };
 
-    cin.get();
+return 0;
 };
