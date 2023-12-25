7e032ebf7ac66fb8e14015814a2da5a835b7d7c9
kaibas01 <82393336+benedekaibas@users.noreply.github.com>
2023-12-25 05:48:09 -0500

    Delete cppnew/test.py

--- cppnew/test.py
+++
@@ -1,9 +0,0 @@
-def main():
-    list_one = list(input('Enter three numbers: '))
-    list_two = list(input('Etner three numbers: '))
-    print(f"{list_one}\n{list_two}")
-  
-    
-        
-if __name__ == "__main__":
-    main()
