b740b646bf48b39c299d408c45630ca5c8854542
kaibas01 <kaibas01@allegheny.edu>
2024-01-05 08:30:53 -0500

    updated code

--- cppnew/test.cpp
+++ cppnew/test.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-double square_s2(double s2){
+int square_s2(int s2){
     return s2 * s2;
 }
 
-void print_s2(double s2){
+void print_s2(int s2){
     cout << "s2 squared gives the result " << s2 << endl;
 }
 
