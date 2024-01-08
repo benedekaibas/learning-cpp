c0169a4f03efe30c3c480b1fa71441668ebb3298
kaibas01 <kaibas01@allegheny.edu>
2024-01-08 15:48:48 -0500

    checking sizeof in code

---
+++ cppnew/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    name = "Benedek Kaibas";
+    long a;
+    a = 123;
+    long long b;
+    b = 984324;
+    int c;
+    c = 43;
+    cout << "Size of name " << sizeof(name) << endl;
+    cout << "Size of long a " << sizeof(a) << endl;
+    cout << "Size of long long b " << sizeof(b) << endl;
+    cout << "Size of int c " << sizeof(c) << endl;
+
+
+
+
+
+
+}
