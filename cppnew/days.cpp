1e6e585d579f2ab186940c9a3054201af44fa3e7
kaibas01 <kaibas01@allegheny.edu>
2024-01-08 07:49:05 -0500

    practice

---
+++ cppnew/days.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <string>  // Include the <string> header for using the string type
+
+
+int main(){
+    string userDay;
+    cout << "enter a day: ";
+    cin >> userDay;
+
+    cout << "the user entered the following: " << userDay;
+    int intDay = stoi(userDay);
+
+    switch(intDay){
+        case 
+
+    }
+
+}
