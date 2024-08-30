4adc90789e35fd6f52ca9596dd58bdd77a02cc39
benedekaibas <kaibas.benedek02@gmail.com>
2024-08-30 15:24:49 -0400

    started a more complicated and sufficient way of calculator

--- cppnew/calculator.cpp
+++ cppnew/calculator.cpp
@@ -5,32 +5,22 @@ Write a simple calculator in c++
 #include <iostream>
 using namespace std;
 
+int add(number_one, number_two){
 
-int main(){
-    cout << "Hello User!" << "\n";
-
-
-    float a, b;
-    cout << "Enter a number: ";
-    cin >> a;
-    cout << "Enter another number: ";
-    cin >> b;
-    string operand;
-    cout << "Enter an operand that you would like to do: ";
-    cin >> operand;
-    if (operand == "+"){
-        cout << "result:" << a + b;
-    } else if (operand == "-"){
-        cout << "result" << a - b;
-    } else if (operand == "/"){
-        cout << "result"  << a / b;
-    } else if (operand == "*"){
-        cout << "result" << a * b;
-    }else {
-        cout << "You did not enter anything or not an operand";
-    };
-
-
-    cin.ignore();
-    return 0;
+    return number_one + number_two;
 };
+
+int substract(number_one, number_two){
+
+    return number_one - number_two;
+}
+
+int multiply(number_one, number_two){
+
+    return number_one * number_two;
+}
+
+int divide(number_one, number_two){
+
+    return number_one / number_two;
+}
