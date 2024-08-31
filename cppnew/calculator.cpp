7f2e09690dbe35f60a982d8ed5126a44efeac8f4
benedekaibas <kaibas.benedek02@gmail.com>
2024-08-31 00:17:52 -0400

    calculator well defined

--- cppnew/calculator.cpp
+++ cppnew/calculator.cpp
@@ -3,24 +3,36 @@ Write a simple calculator in c++
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int add(number_one, number_two){
+int add(int number_one, int number_two);
+int substract(int number_one, int number_two);
+int multiply(int number_one, int number_two);
+int divide(int number_one, int number_two);
 
+int add(int number_one, int number_two){
     return number_one + number_two;
 };
 
-int substract(number_one, number_two){
-
+int substract(int number_one, int number_two){
     return number_one - number_two;
-}
+};
 
-int multiply(number_one, number_two){
-
+int multiply(int number_one, int number_two){
     return number_one * number_two;
-}
+};
 
-int divide(number_one, number_two){
+int divide(int number_one, int number_two){
 
+    if (number_two == 0){
+        throw invalid_argument("Can't divide with zero.");
+    }
     return number_one / number_two;
-}
+};
+
+
+
+
+
+
