3001816544c0ee658ca9a673574fc510cd05bb8c
benedekaibas <kaibas.benedek02@gmail.com>
2024-10-08 15:05:15 -0400

    changed the current code

--- cppnew/test.cpp
+++ cppnew/test.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 int main(){
-    string name;
-    name = "Benedek Kaibas";
-    cout << name;
-    int newName;
-    newName = 321;
-    long a;
-    a = 123;
-    long long b;
-    b = 984324;
-    int c;
-    c = 43;
-    cout << "Size of name " << sizeof(name) << endl;
-    cout << "Size of long a " << sizeof(a) << endl;
-    cout << "Size of long long b " << sizeof(b) << endl;
-    cout << "Size of int c " << sizeof(c) << endl;
+    int i = 0;
+    list<int> numbers;
 
-    //TODO: make this more readable
-    if (a >= newName ){
-        cout << "Size of long a " << sizeof(a) << endl;
-    } else{
-        cout << "not working";
+    while(i < 10){
+        i++;
+
+        if(i > 0 && i % 2 != 0){
+            numbers.push_front(i);
+
+            cout << "These are the odd numbers: ";
+            for(int num : numbers){
+                cout << num << " " << endl;
+            }
+        } else {
+            cout << "These are the even numbers: " << i;
+        }
+        
     }
-
+    return 0;
 }
