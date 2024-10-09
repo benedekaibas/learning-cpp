194cc4c19c840ab9c3f22f71d2e75a8e8e9c0035
benedekaibas <kaibas.benedek02@gmail.com>
2024-10-09 11:11:34 -0400

    done with the code

--- cppnew/test.cpp
+++ cppnew/test.cpp
@@ -5,18 +5,17 @@ using namespace std;
 int main(){
     int i = 0;
     list<int> numbers;
-
     while(i < 10){
         i++;
 
         if(i > 0 && i % 2 != 0){
             numbers.push_back(i);
-    
+    }}
+
     cout << "List contains: ";
     for(int num : numbers){
-        cout << num;
+        cout << num;    
     }
     cout << endl;
-    
-    }
-}}
+
+}
