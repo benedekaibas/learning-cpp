520b37fe5ee9d81c3907ab91ff6086618895f676
benedekaibas <kaibas.benedek02@gmail.com>
2024-10-09 23:14:35 -0400

    code is working

--- cppnew/auto.cpp
+++ cppnew/auto.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
     // auto a; this line will give error
--- cppnew/test.cpp
+++ cppnew/test.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
-#include <list>
 using namespace std;
 
+//Title: Game	Number Guessing	
+//Description: Write a program that generates a random number between 1 and 100 and asks the user to guess the number. 
+//Provide feedback to the user (e.g., "Too high" or "Too low") until they guess the correct number.	
+
 int main(){
-    int i = 0;
-    list<int> numbers;
-    while(i < 10){
-        i++;
+    int random_number = rand() % 100 + 1;
+    int guess;
+    cout << random_number;
 
-        if(i > 0 && i % 2 != 0){
-            numbers.push_back(i);
-    }}
+    cout << "Guess a number between 1 and 100: ";
+    cin >> guess;
+    
+    while(guess != random_number){
 
-    cout << "List contains: ";
-    for(int num : numbers){
-        cout << num;    
-    }
-    cout << endl;
+        if(guess > random_number){
 
-    for(int num : numbers){
-        if(num > 3){
-            cout << num;
+            cout << "Your guess is higher than the guess number" << endl;
+            cout << "Your guess is not correct! Guess again: ";
+            cin >> guess;
+            if(guess == random_number){
+                cout << "You guessed the correct number: " << random_number;
         }
+        }else{
+            cout << "Your guess is lower than the guess number" << endl;
+            cout << "Your guess is not correct! Guess again: ";
+            cin >> guess;
+            if(guess == random_number){
+                cout << "You guessed the correct number: " << random_number;
+        }
+        }
+
+
+         
     }
-    cout << endl;
 }
