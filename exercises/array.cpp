65dfd7cd0338b7190cdaa23d0a189530f3a3338c
benedekaibas <kaibas.benedek02@gmail.com>
2024-11-01 10:44:29 -0400

    logic rewritten

--- exercises/array.cpp
+++ exercises/array.cpp
@@ -2,6 +2,9 @@
 Title: Find the Second Smallest Element in an Array using Functions
 Description: Write a program that asks the user to enter an array of 
 integers and finds the second smallest element in the array using a function.
+
+g++ -std=c++14 -o array array.cpp
+
 */
 
 #include <iostream>
@@ -11,12 +14,14 @@ using namespace std;
 
 void user_input(vector<int> &user_array){
     // vector<int> user_array;
-    int number;
-    cout << "Enter integer numbers: ";
+    int number;    
+    cout << "Enter integers";
 
-    while(cin >> number){
+    while(cin >> number && user_array.size() < 10){
+        cout << "Enter integers";
         user_array.push_back(number);
     }
+
     cout << "The array you have entered is the following: ";
     for (int num : user_array){
         cout << num << " ";
