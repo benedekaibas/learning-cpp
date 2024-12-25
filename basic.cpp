f695e49ddcb74e6fb5c57e6e2952c21d8f43dc92
benedekaibas <kaibas.benedek02@gmail.com>
2024-12-26 00:05:03 +0100

    started new file

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int integer_value = 4;
+    bool boolean_value = true;
+    string string_value = "ABC";
+    char char_value = 'A';
+    double double_value = 87.43;
+
+    cout << integer_value;
+}
