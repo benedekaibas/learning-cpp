1840a3824bfe17f8438316bc97bf8d5550208b36
benedekaibas <kaibas.benedek02@gmail.com>
2024-12-28 21:43:38 +0100

    new file created

--- basic.cpp
+++ basic.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    return 0;
+/*
+Exercise 1: Number Comparison
+Write a program that takes two
+numbers and prints which one is larger.
+*/
+
+int main(){
+    int number_one;
+    int number_two;
+
+    cout << "Enter a number: " << endl;
+    cin >> number_one;
+    cout << "Enter the second number: " << endl;
+    cin >> number_two;
 }
