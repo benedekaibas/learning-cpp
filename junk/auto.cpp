71198c89a76bdee847c0d425cf7449d7f1b5bb26
benedekaibas <kaibas.benedek02@gmail.com>
2025-02-16 22:56:16 -0500

    swap implemented

--- junk/auto.cpp
+++ junk/auto.cpp
@@ -1,40 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    // auto a; this line will give error
-    // because 'a' is not initialized at
-    // the time of declaration
-    // a=33;
- 
-    // see here x ,y,ptr are
-    // initialised at the time of
-    // declaration hence there is
-    // no error in them
-    auto x = 4;
-    auto y = 3.37;
-    auto z = 3.37f;
-    auto c = 'a';
-    auto ptr = &x;
-    auto pptr = &ptr; //pointer to a pointer
-    cout << typeid(x).name() << endl
-         << typeid(y).name() << endl
-         << typeid(z).name() << endl
-         << typeid(c).name() << endl
-         << typeid(ptr).name() << endl
-           << typeid(pptr).name() << endl;
- 
-    cin.get();
+int main(){
 
+  int a = 5;
+  int b = 9;
+
+  cout << "A is: " << a << "B is: " << b << endl;
+
+  a = b;
+  b = a;
+
+  cout << "A is: " << a << "B is: " << b << endl;  
+
+  return 0;
 }
-// the output
 
-/*
-i
-d
-f
-c
-Pi
-PPi
-*/
