7dfb74ad54c0c7a1f40a17e32815d8906e642ff1
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-01 23:33:53 -0500

    changes

--- junk/practice.cpp
+++ junk/practice.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
-
+#include <vector>
 
 //output of two given numbers
 
 int main(){
-
+    vector<int> this_arr;
     int array[10] = {1,2,3,4,5,6,7,8,9,10};
+
     for(int i : array){
-        cout << i << endl;
+        if ( i % 2 == 0){
+            this_arr.push_back(i);
     }
-    //cout << array << endl;
+    cout << "Elements in the new array: ";
+    for (int i : this_arr){
+        cout << i << " ";
+    }
+
     int* ptr = array;
     cout << ptr << endl;
 
     return 0;
 
-}
+}}
