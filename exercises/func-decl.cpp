409ee1ed7e2c5f9151dab76df89aee6feb1171dc
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-13 22:08:10 -0500

    started implementing the functions for the study

--- exercises/func-decl.cpp
+++ exercises/func-decl.cpp
@@ -5,6 +5,19 @@ void addition(int a, int b){
     cout << a + b << endl;
 }
 
+int addition_int(int a, int b){
+    return a + b;
+}
+
+float addition_float(float a, float b){
+    return a + b;
+}
+
+string add_strings(string word_one, string word_two){
+    return word_one + " " + word_two;
+}
+
 int main(){
     addition(3,5);
+    
 }
