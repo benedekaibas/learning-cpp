b6763d3b07fc35942864ba514b4bb0deec16e18f
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-15 18:47:15 -0500

    changes

--- basic.cpp
+++ basic.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
 using namespace std;
 
-/**
-   Write a C++ program that takes two numbers as input and prints which one is larger, 
-   or if they are equal.
- */
+void data_type(){
+    int a;
+    char b = 'c';
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+}
+
 
 int main(){
-    
-    int number_one;
-    int number_two;
-
-    cout << "Enter a number: ";
-    cin >> number_one;
-
-    cout << "Enter another number: ";
-    cin >> number_two;
-
-    if(number_one > number_two){
-        cout << "Number one is larger: " << number_one << endl;
-    } else if (number_one < number_two){
-        cout << "Number two is larger: " << number_two << endl;
-    } else {
-        cout << number_one << "and" << number_two << "are equal" << endl;
-    }
-    return 0;
+    cout << "Hello World" << endl;
+    data_type();
 }
--- exercises/func-decl.cpp
+++ exercises/func-decl.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void addition(int a, int b){
-    cout << a + b << endl;
+int factorial(int n) {
+    return n * factorial(n - 1);
 }
 
-int addition_int(int a, int b){
-    return a + b;
-}
-
-float addition_float(float a, float b){
-    return a + b;
-}
-
-string add_strings(string word_one, string word_two){
-    return word_one + " " + word_two;
-}
-
-int main(){
-    addition(3,5);
-    cout << addition_int(4,5) << endl;
-    cout << addition_float(4.3, 6.6) << endl;
-    cout << add_strings("Hello", "World") << endl;
-
+int main() {
+    cout << "Factorial of 5: " << factorial(5) << endl; // Should print 120
+    return 0;
 }
