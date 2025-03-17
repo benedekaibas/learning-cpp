787f405d3816dde50275b624818b424addc91068
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-16 22:43:15 -0500

    changed files

--- exercises/fahr_to_cels.cpp
+++ exercises/fahr_to_cels.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int a(int b){
-    
+float dot_product(float a[], float b[], int n) {
+    float result = 0.0;
+    for (int i = 0; i < n; i++) {
+        result += a[i] * b[i]; // Manual element-wise multiplication and summation
+    }
+    return result;
 }
 
 int main(){
-    float fahrenheit = 98.8;
-    float result = a(fahrenheit);
+    float a[] = {1.0, 2.0, 3.0};
+    float b[] = {4.0, 5.0, 6.0};
+    int n = 3;
+
+    float result = dot_product(a, b, n);
     cout << result;
 }
