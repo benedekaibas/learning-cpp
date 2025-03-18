ba925480f4e71fb2f765b3e2b1fe7b7d1866a8e1
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-18 11:12:08 -0500

    references updated

--- exercises/references.cpp
+++ exercises/references.cpp
@@ -6,10 +6,15 @@ int addition(int a, int b){
     return a + b;
 }
 
-int main(){
-    int a = 321;
-    int b = 10;
-    const int &result = addition(a, b);
+int formula(int c, int d){
+    const int &ref = addition(9, 8);
+    
+    return (c + d) - ref;
+}
 
-    cout << "The sum of the two numbers is: " << result << endl;
+int main(){
+    int result = formula(9,8);
+
+    cout << result;
+
 }
