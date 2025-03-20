5f01a35d8f0ee0f9e17938908790c869a2c268e1
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-20 11:22:09 -0500

    changes in the code

--- basic.cpp
+++ basic.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 //Write a C++ program that uses a for loop to calculate the sum of all even numbers between 1 and 100.
 
-void pass_array(int my_array[5]){
-    for (int i = 0; i < 5; i++){
-        cout << my_array[i];
-    }
+int ptr_func(int *a, int *b){
+    return *a * *b;
 }
 
 int main(){
-    int my_numbers[5] = {1,2,3,4,5};
-    pass_array(my_numbers);
+    int a = 9;
+    int b = 10;
+
+    int *ptr_a = &a;
+    int *ptr_b = &b;
+
+    int prod = ptr_func(ptr_a, ptr_b);
+    cout << prod;
 }
