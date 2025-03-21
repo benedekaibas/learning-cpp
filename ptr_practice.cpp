4c5434a11259e829de63bfe5eaadf3d903d8ee24
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-21 09:41:34 -0500

    started file

--- ptr_practice.cpp
+++ ptr_practice.cpp
@@ -2,18 +2,3 @@
 #include <vector>
 using namespace std;
 
-int find_max(vector <int> arr){
-    for (int i = 1; i < arr.size(); i++){
-        if(i > arr[0]){
-            return arr[i];
-        }
-    }
-}
-
-int main(){
-    vector<int> this_arr = {1,2,3,4,5,6,7};
-    int result = find_max(this_arr);
-    cout << result;
-
-    return 0;
-}
