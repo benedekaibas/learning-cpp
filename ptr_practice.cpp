e5a98d56b0ab7f6894c8b23676a392d0360b19de
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-20 22:39:53 -0500

    started ptr practice assignment still issue with vector max element finder

---
+++ ptr_practice.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int find_max(vector <int>arr, int i){
+    for (int i = 0; i > arr.size(); i++){
+        return arr.max(i);
+    }
+}
