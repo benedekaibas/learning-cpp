9fac372880a0fccb8477bf7291b3a948b78e8e35
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-20 23:04:10 -0500

    out of control issue

--- ptr_practice.cpp
+++ ptr_practice.cpp
@@ -2,8 +2,18 @@
 #include <vector>
 using namespace std;
 
-int find_max(vector <int>arr, int i){
-    for (int i = 0; i > arr.size(); i++){
-        return arr.max(i);
+int find_max(vector <int> arr){
+    for (int i = 1; i < arr.size(); i++){
+        if(i > arr[0]){
+            return arr[i];
+        }
     }
+}
+
+int main(){
+    vector<int> this_arr = {1,2,3,4,5,6,7};
+    int result = find_max(this_arr);
+    cout << result;
+
+    return 0;
 }
