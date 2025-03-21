1c10b39f9e8930c6f01f3acf78b71ddebc8ee5a6
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-21 10:23:39 -0500

    started working on the assignment

--- ptr_practice.cpp
+++ ptr_practice.cpp
@@ -7,3 +7,17 @@ Write a program that asks the user to enter integers as inputs to be stored in t
 There are also two integer pointers named ptrA and ptrB. Assign the values of 'a' and 'b' to ptrA and ptrB respectively,
 and display them.
 */
+
+int user_input(int usr_ip_one, int usr_ip_two){
+    cout << "Enter an integer: " << endl;
+    cin >> usr_ip_one;
+
+    cout << "Enter another integer: " << endl;
+    cin >> usr_ip_two;
+
+    return usr_ip_one, usr_ip_two;
+}
+
+int input_ptr(int *ptrA, int *ptrB){
+    
+}
