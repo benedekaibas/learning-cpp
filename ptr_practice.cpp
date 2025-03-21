a5e2f85a0284c404cd5370837377e4b4f72eb61f
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-21 09:42:14 -0500

    description of task added

--- ptr_practice.cpp
+++ ptr_practice.cpp
@@ -2,3 +2,8 @@
 #include <vector>
 using namespace std;
 
+/*
+Write a program that asks the user to enter integers as inputs to be stored in the variables 'a' and 'b' respectively. 
+There are also two integer pointers named ptrA and ptrB. Assign the values of 'a' and 'b' to ptrA and ptrB respectively,
+and display them.
+*/
