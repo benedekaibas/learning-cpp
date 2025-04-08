2b1d236d58e3d2151b9d455589e7bc1fb239d23c
benedekaibas <kaibas.benedek02@gmail.com>
2025-04-08 11:16:50 -0500

    function works perfectly

--- junk/ptr_arr.cpp
+++ junk/ptr_arr.cpp
@@ -8,6 +8,16 @@ the sum of all elements in an array using pointers.
 
 void sum_array(int *arr, int size){
     int this_arr[5] = {1,2,3,4,5};
-    int *arr = &this_arr[5];
+    int sum = 0;
+    for(int i : this_arr){
+        sum += i;
+    }
+    cout << sum;
+}
 
+int main(){
+    int my_array[5] = {1,2,3,4,5};
+    int *ptr = &my_array[5];
+
+    sum_array(ptr, 5);
 }
