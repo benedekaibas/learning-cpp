0d5b05d56c5307782844ca6234f0c04814ecd050
benedekaibas <kaibas.benedek02@gmail.com>
2025-04-27 00:22:28 -0500

    play with array

--- ptr_practice.cpp
+++ ptr_practice.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+
 #include <vector>
 using namespace std;
 
 
-int create_vector(array<int> new_array){
+vector<int> create_vector(vector<int> new_array){
   //new_array = {1,2,3,4,5,6,7,8,9};
   return new_array;
 }
 
 int main(){
-  int this_arr = {1,2,3,4,5,6,7,8,9};
-  arr = create_vector(this_arr);
-  cout << arr;
+  vector<int> this_arr = {1,2,3,4,5,6,7,8,9};
+  vector<int> arr = create_vector(this_arr);
+
+  arr.erase(arr.begin(), 2);
+
+  for (int i : arr){
+    cout << i;
+  }
 }
