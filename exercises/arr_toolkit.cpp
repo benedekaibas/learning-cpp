64068e3b867e5459bc772692f1f9aa4b49c09477
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-04 16:51:43 -0500

    dumped core bug removed and started working on filling array function

--- exercises/arr_toolkit.cpp
+++ exercises/arr_toolkit.cpp
@@ -8,16 +8,26 @@ int* arr_alloc(int *arr_ptr){
   return arr_ptr;
 }
 
+void fill_array(vector<int> &arr, int *i){
+  arr.push_back(*i);
+}
+
+int main(){
+  vector<int> simple_arr = {1,2,3,4,5,6};
+  vector<int> *arr = &simple_arr;
+  int counter = 0;
+  int *i = &counter;
+  fill_array(vector<int> arr, *i);
+}
+
+/*
 int main(){
   int arr[5] = {1,2,3,4,5};
   int *ptr = &arr[5];
-  int* result = arr_alloc(ptr);
+  int *result = arr_alloc(ptr);
 
-  for (int *i = 0; i < result; i++){
-    while (*i < 5){
-      cout << i;
-    }
-
+  for (int i = 1; i < sizeof(result); i++){
+    cout << i;
   }
 }
-
+*/
