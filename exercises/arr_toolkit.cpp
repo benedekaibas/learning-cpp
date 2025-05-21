deea0b4e6030a8d319e4bc4ad75ff18a8930de54
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-21 23:54:25 +0200

    finished first function & added file

--- exercises/arr_toolkit.cpp
+++ exercises/arr_toolkit.cpp
@@ -8,18 +8,19 @@ int* arr_alloc(int *arr_ptr){
   return arr_ptr;
 }
 
-void fill_array(vector<int> *arr){
+void fill_array(vector<int> arr){
   for(int i = 0; i < 10; i++){
-    vector<int> new_arr = arr[i];
+    //vector<int> new_arr = arr[i];
+    vector<int> new_arr;
     for (int i : new_arr){
-      cout << i; //this results in memory allocation issue
+      new_arr.push_back(i); //this results in memory allocation issue
     }
   }
 }
 
 int main(){
-  vector<int> this_arr[10] = {};
-  vector<int> *ptr = &this_arr[10];
+  vector<int> this_arr = {};
+  vector<int> ptr = this_arr;
   fill_array(ptr);
 
 }
---
+++ exercises/classes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+class MyClass{
+
+public:
+    int val = 4;
+
+    void show(){
+      cout << "Value: " << val << endl;
+    }
+
+};
+
+
+int main(){
+  MyClass();
+
+}
