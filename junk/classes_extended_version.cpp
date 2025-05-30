61a4e7d80971e56a8aa499364cfa2eaffc4718ff
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-30 12:56:56 +0200

    files organized

---
+++ junk/classes_extended_version.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <ostream>
+
+
+class MyClass {
+private:
+    int x;
+public:
+    MyClass(int val) : x(val) {
+      std::cout << val << std::endl;
+    };
+};
+
+class YourClass{
+private:
+  int x = 20;
+public:
+  YourClass(){
+    std::cout << x << std::endl;
+  };
+};
+
+class PtrUse{
+private:
+  int val = 30;
+public:
+  PtrUse(){
+    int* ptr = &val;
+    std::cout << ptr << std::endl;
+    std::cout << *ptr << std::endl;
+  };
+};
+
+class Vector{
+public:
+  Vector(int s):elem{new double[s]}, sz{s}{};
+  double& operator[](int i){return elem[i];}
+  int size(){return sz;}
+private:
+  double* elem;
+  int sz;
+  };
+
+
+enum ClassYears{
+  freshman = 1,
+  sophomore = 2,
+  junior = 3,
+  senior = 4,
+};
+
+int main(){
+  MyClass(10);
+  YourClass obj;
+  PtrUse obj1;
+  Vector v(6);
+  enum ClassYears classes;
+  classes = freshman;
+  std::cout << classes << std::endl;
+}
