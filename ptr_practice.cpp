e31f7e6178ecd3842993d21f74b48f5a2f9d3c2f
benedekaibas <kaibas.benedek02@gmail.com>
2025-06-12 18:36:50 +0200

    buffer overflow controlled

--- ptr_practice.cpp
+++ ptr_practice.cpp
@@ -7,6 +7,14 @@ std::vector<int> create_vector(std::vector<int> new_array){
   return new_array;
 }
 
+void buffer_vector(std::vector<int> arr){
+  arr.push_back(10);
+
+  for(int i : arr){
+    std::cout << i << std::endl;
+  }
+}
+
 int main(){
   std::vector<int> this_arr = {1,2,3,4,5,6,7,8,9};
   std::vector<int> arr = create_vector(this_arr);
@@ -16,4 +24,6 @@ int main(){
   for (int i : arr){
     std::cout << i << std::endl;
   }
+  std::cout << "\n";
+  buffer_vector(this_arr);
 }
