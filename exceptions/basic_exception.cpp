5e88a941740640086fcbc67af70170d4d884210d
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-03 15:50:17 +0200

    edit: done with file

---
+++ exceptions/basic_exception.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+/*
+   Write a program that asks the user to input two integers, then divides the first number by the second.
+   If the second number is zero, throw an exception and handle it gracefully.
+*/
+
+int input(int x, int y) {
+  /*
+  std::cout << "Enter an integer: " << '\n';
+  std::cin >> x;
+
+  std::cout << "Enter another integer" << '\n';
+  std::cin >> y;
+  */
+  try {
+    if (y != 0) {
+      return x / y;
+    }
+    else {
+      throw(y);
+    }
+  }
+  catch(int division_by_zero) {
+    std::cout << "Division by zero happened" << "" << division_by_zero << '\n';
+  }
+}
+
+int main() {
+  int x, y;
+  std::cout << "Enter an integer: " << '\n';
+  std::cin >> x;
+
+  std::cout << "Enter another integer: " << '\n';
+  std::cin >> y;
+  std::cout << input(x, y);
+}
