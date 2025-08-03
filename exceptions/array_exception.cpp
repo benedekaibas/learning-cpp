b4bdba3d36ee46ffc0f061bdfba1067d03632cc8
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-03 16:39:42 +0200

    edit: new file created

---
+++ exceptions/array_exception.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/*
+   Define an array of 5 integers (e.g., {10, 20, 30, 40, 50}).
+  Prompt the user to input an index.
+  If the index is within bounds, print the element.
+  If the index is out of range, throw a std::out_of_range exception.
+  Catch the exception and print an error message.
+*/
+
+
