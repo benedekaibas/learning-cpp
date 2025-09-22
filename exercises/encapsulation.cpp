f38da5b43bd8e30990289bd53dd24f6f86cd7457
benedekaibas <kaibas.benedek02@gmail.com>
2025-09-22 10:18:13 -0400

    style: new file for encapsulation added

---
+++ exercises/encapsulation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class Person {
+private:
+  int socialID;
+  std::string name;
+
+public:
+  Person(std::string n, int id) : name(n), socialID(id) {};
+
+  const std::string getName() {
+    return name;
+  }
+
+  void setName(std::string newName) {
+    name = newName;
+    std::cout << newName;
+  }
+
+  bool validateID() {
+    if(socialID < 0 || socialID > 1001) {
+      return "Invalid ID!";
+    } else {
+      std::cout << "Valid ID!";
+      return socialID;
+    }
+  }
+};
+
+int main() {
+  Person personObj("Benedek", 15);
+  personObj.validateID();
+  personObj.setName("Adam");
+
+} 
